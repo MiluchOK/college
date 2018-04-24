278cd974053347751f57714b38f570de2f40a41e
alexey <amilyukov@amilyukovbranchio.local>
2018-04-24 15:40:37 -0700

    test

--- F021A/assignment4/assignment4.py
+++ F021A/assignment4/assignment4.py
@@ -24,27 +24,35 @@ num2words = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', \
             11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', \
             15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen'}
 num2words2 = ['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
-def spell(num):
+
+def n2w(num):
+    if num < 0:
+        print('1')
+        return "minus " + n2w(num * -1)
     if num == 0:
+        print('2')
         return ""
     if num < 20:
+        print('3')
         return (num2words[num])
     elif num < 100:
+        print('4')
         ray = divmod(num,10)
-        return (num2words2[ray[0]-2]+" "+spell(ray[1]))
+        return (num2words2[ray[0]-2]+" "+n2w(ray[1]))
     elif num <1000:
+        print('5')
         ray = divmod(num,100)
-        if ray[1] == 0:
-            mid = " hundred"
-        else:
-            mid =" hundred and "
-        return(num2words[ray[0]]+mid+spell(ray[1]))
-    elif num < 10000:
+        mid = " hundred "
+        return(num2words[ray[0]]+mid+n2w(ray[1]))
+    elif num < 1000000:
+        print('6')
         ray = divmod(num, 1000)
-        if ray[1] == 0:
-            mid = " thousand"
-        else:
-            mid = " thousand and "
-        return(num2words[ray[0]]+mid+spell(ray[1]))
+        mid = " thousand "
+        return(n2w(ray[0])+ mid +n2w(ray[1]))
+    elif num < 1000000000:
+        print('7')
+        ray = divmod(num, 1000000)
+        mid = " million "
+        return(n2w(ray[0])+ mid +n2w(ray[1]))
 
-print(spell(9999))
+print(n2w(9999))
