fffdee80b9016619de73265218ebfb02dfb6c375
alexey <amilyukov@amilyukovbranchio.local>
2018-04-24 16:49:56 -0700

    #4 is up for refactoring

--- F021A/assignment4/test_assignment4.py
+++ F021A/assignment4/test_assignment4.py
@@ -6,11 +6,19 @@ from num2words import num2words
 class Assignment4Test(unittest.TestCase):
 
     def test_can_convert_all_the_nums(self):
-        for number in range(-999999999, 999999999, 1153):
+        for number in range(-999999999, 999999999, 19153):
             print("Testing {}".format(number))
             expected = num2words(number).replace(",", "").replace("-", " ").replace(" and ", " ").strip()
             actual = n2w(number).replace("  ", " ").strip()
             self.assertEqual(expected, actual)
+
+    def test_can_convert_small_nums(self):
+        for number in range(-1999999, 1999999, 9):
+            print("Testing {}".format(number))
+            expected = num2words(number).replace(",", "").replace("-", " ").replace(" and ", " ").strip()
+            actual = n2w(number).replace("  ", " ").strip()
+            self.assertEqual(expected, actual)
+
 
 
 if __name__ == '__main__':
