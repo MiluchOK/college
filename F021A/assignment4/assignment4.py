6c5c28837db3717f8cc4f9f4d9137d6cb67a7c11
MiluchOK <milyukov.alexey@gmail.com>
2018-04-24 20:50:54 -0700

    Refactoring

--- F021A/assignment4/assignment4.py
+++ F021A/assignment4/assignment4.py
@@ -1,51 +1,58 @@
+"""
+The program takes a whole number as parameter and returns a string containing the number n2wed out in English
+"""
+
+# Hash for number - word mapping
+num2words = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
+             6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
+             11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen',
+             15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen',
+             20: 'twenty', 30: 'thirty', 40: 'forty', 50: 'fifty', 60: 'sixty', 70: 'seventy',
+             80: 'eighty', 90: 'ninety'
+             }
+
+# Array for tens
+# num2words2 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
 
-# def n2w(n):
-
-    # int_map = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
-    #            6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
-    #            11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen',
-    #            15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen',
-    #            19: 'nineteen', 20: 'twenty', 30: 'thirty', 40: 'forty',
-    #            50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty',
-    #            90: 'ninety'}
-    #
-    # try:
-    #     return int_map[n]
-    # except KeyError:
-    #     try:
-    #         return int_map[n - n % 10] + ' ' + int_map[n % 10].lower()
-    #     except KeyError:
-    #         raise RuntimeError('Number out of range {}'.format(n))
-
-
-num2words = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', \
-            6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten', \
-            11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', \
-            15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'}
-num2words2 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
-
+# The core n2wing algorithm
 def n2w(num):
     if num < 0:
         return "minus " + n2w(num * -1)
-    if num == 0:
+    elif num == 0:
         return ""
-    if num < 20:
-        return (num2words[num])
+    elif num < 20:
+        return num2words[num]
     elif num < 100:
-        ray = divmod(num,10)
-        return (num2words2[ray[0]-2]+" "+n2w(ray[1]))
-    elif num <1000:
-        ray = divmod(num,100)
+        # Kind of a special kind
+        quotient, remainder = divmod(num, 10)
+        return num2words[quotient*10] + " " + n2w(remainder)
+    elif num < 1000:
+        ray = divmod(num, 100)
         mid = " hundred "
-        return(num2words[ray[0]]+mid+n2w(ray[1]))
+        return num2words[ray[0]] + mid + n2w(ray[1])
     elif num < 1000000:
-        ray = divmod(num, 1000)
-        mid = " thousand "
-        return(n2w(ray[0])+ mid +n2w(ray[1]))
+        return broker(num, 1000, "thousand")
     elif num < 1000000000:
-        ray = divmod(num, 1000000)
-        mid = " million "
-        return(n2w(ray[0])+ mid +n2w(ray[1]))
+        return broker(num, 1000000, "million")
 
-print(n2w(9999))
+
+# The broker logic
+def broker(num, grouper, gluer):
+    quotient, remainder = divmod(num, grouper)
+    return n2w(quotient) + (" {} ".format(gluer)) + n2w(remainder)
+
+
+# The program gateway for input validation and optimizations
+def spell(num):
+    max_input = 1000000000000
+    min_input = max_input * -1
+
+    if num == 0:
+        return "zero"
+    elif num > max_input:
+        raise ValueError("Invalid input. The value should be no more than {}".format(max_input))
+    elif num < min_input:
+        raise ValueError("Invalid input. The value should be no less than {}.".format(min_input))
+    else:
+        return n2w(num)
--- F021A/assignment4/test_assignment4.py
+++ F021A/assignment4/test_assignment4.py
@@ -1,23 +1,32 @@
 import unittest
-from assignment4 import n2w
+from assignment4 import spell
 from num2words import num2words
 
 
 class Assignment4Test(unittest.TestCase):
 
     def test_can_convert_all_the_nums(self):
-        for number in range(-999999999, 999999999, 19153):
-            print("Testing {}".format(number))
+        for number in range(-999999999, 999999999, 191531):
             expected = num2words(number).replace(",", "").replace("-", " ").replace(" and ", " ").strip()
-            actual = n2w(number).replace("  ", " ").strip()
+            actual = spell(number).replace("  ", " ").strip()
             self.assertEqual(expected, actual)
 
     def test_can_convert_small_nums(self):
-        for number in range(-1999999, 1999999, 9):
-            print("Testing {}".format(number))
+        for number in range(-1999999, 1999999, 191):
             expected = num2words(number).replace(",", "").replace("-", " ").replace(" and ", " ").strip()
-            actual = n2w(number).replace("  ", " ").strip()
+            actual = spell(number).replace("  ", " ").strip()
             self.assertEqual(expected, actual)
+
+    def test_can_convert_tiny_nums(self):
+        for number in range(-99, 99):
+            expected = num2words(number).replace(",", "").replace("-", " ").replace(" and ", " ").strip()
+            actual = spell(number).replace("  ", " ").strip()
+            self.assertEqual(expected, actual)
+
+    def test_can_return_zero(self):
+        expected = "zero"
+        actual = spell(0)
+        self.assertEqual(expected, actual)
 
 
 
