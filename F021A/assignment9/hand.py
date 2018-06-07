374419d450d9c01a5cd560479c077d6c18e57140
MiluchOK <milyukov.alexey@gmail.com>
2018-06-06 22:16:55 -0700

    Fixing cards generation

--- F021A/assignment9/hand.py
+++ F021A/assignment9/hand.py
@@ -10,7 +10,7 @@ class Hand:
 	# The constructor takes `numCardsInHand` as param
 	# numCardsInHand is specifying how many cards you want inside the hand
 	def __init__(self, num_cards_in_hand):
-		self.cards = [Card.get_random for i in range(num_cards_in_hand)]
+		self.cards = [Card.get_random() for i in range(num_cards_in_hand)]
 
 	# Add a new random card to hand
 	def hit_me(self):
