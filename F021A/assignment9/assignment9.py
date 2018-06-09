cde8cfe0a361b14b99c5ceb2c96e0d0db6dfe1aa
MiluchOK <milyukov.alexey@gmail.com>
2018-06-08 23:07:38 -0700

    Adding beef to main

--- F021A/assignment9/assignment9.py
+++ F021A/assignment9/assignment9.py
@@ -4,26 +4,19 @@ The main application that thoroughly tests class Hand
 
 from hand import Hand
 
-new_hand = Hand(10)
+new_hand = Hand(3)
 print(new_hand)
 print(new_hand.bj_value())
 file_name = 'testing.json'
 new_hand.save(file_name)
 reloaded_hand = Hand.load_from_disk(file_name)
-print(reloaded_hand.cards)
 print(reloaded_hand)
 print(reloaded_hand.bj_value())
-#
-#
-# print("\n Adding a new card: \n")
-#
-# new_hand.hit_me()
-# print(new_hand)
-# print(new_hand.bj_value())
-#
-# print("\n Making a new Hand: \n")
-# very_new_hand = Hand(100)
-# print(very_new_hand)
-# print(very_new_hand.bj_value())
-
-
+print("\n Adding a new card: \n")
+new_hand.hit_me()
+print(new_hand)
+print(new_hand.bj_value())
+new_hand.save(file_name)
+newest_hand = Hand.load_from_disk(file_name)
+print(newest_hand)
+print(newest_hand.bj_value())
