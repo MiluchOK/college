a8c274ec724ed532ed57c4f20c3c398623a1c8eb
MiluchOK <milyukov.alexey@gmail.com>
2018-06-09 22:01:10 -0700

    Finishing up ass9

--- F021A/assignment9/assignment9.py
+++ F021A/assignment9/assignment9.py
@@ -12,7 +12,7 @@ new_hand.save(file_name)
 reloaded_hand = Hand.load_from_disk(file_name)
 print(reloaded_hand)
 print(reloaded_hand.bj_value())
-print("\n Adding a new card: \n")
+print("\n\n Adding a new card: \n")
 new_hand.hit_me()
 print(new_hand)
 print(new_hand.bj_value())
@@ -20,3 +20,35 @@ new_hand.save(file_name)
 newest_hand = Hand.load_from_disk(file_name)
 print(newest_hand)
 print(newest_hand.bj_value())
+
+#OUTPUT
+
+"""
+Eight of Clubs
+Nine of Heart
+Ace of Clubs
+
+18
+Eight of Clubs
+Nine of Heart
+Ace of Clubs
+
+18
+
+
+ Adding a new card: 
+
+Eight of Clubs
+Nine of Heart
+Ace of Clubs
+Jack of Spades
+
+28
+Eight of Clubs
+Nine of Heart
+Ace of Clubs
+Jack of Spades
+
+28
+
+"""
